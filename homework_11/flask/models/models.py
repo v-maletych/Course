3ea20764a9b79fa771601ba5761d4d049459ffc9
Vitalii Maletych <124867918+v-maletych@users.noreply.github.com>
2023-04-07 17:35:31 +0300

    Create models.py

---
+++ homework_11/flask/models/models.py
@@ -0,0 +1,16 @@
+from app import db
+
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+
+
+    @property
+    def serialize(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "body": self.body
+        }
